f669e8eaa033eb7e885dc7db985cc9447ef84584
kitty gonzalez <sechafer@hotmail.com>
2021-05-12 01:15:46 -0300

    Se crea componente item2 y se estrctura lso modulos

--- src/app/modules/creater/creater.module.ts
+++ src/app/modules/creater/creater.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { Item2Component } from '../../componentes/item2/item2.component';
+import { CardaComponent } from '../../componentes/carda/carda.component';
+import { ModifiComponent } from '../../componentes/modifi/modifi.component';
+import { CreaComponent } from '../../componentes/crea/crea.component';
+import { DeleteComponent } from '../../componentes/delete/delete.component';
+import { FormComponent } from '../../componentes/form/form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    Item2Component,
+    CardaComponent,
+    ModifiComponent,
+    CreaComponent,
+    DeleteComponent,
+    FormComponent
+
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    Item2Component,
+    CardaComponent,
+    ModifiComponent,
+    CreaComponent,
+    DeleteComponent,
+    FormComponent
   ]
 })
 export class CreaterModule { }
--- src/app/modules/menu/menu.module.ts
+++ src/app/modules/menu/menu.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ItemComponent } from '../../componentes/item/item.component'
+import { AdmComponent } from '../../componentes/adm/adm.component';
+import { TodoComponent } from '../../componentes/todo/todo.component';
+import { VegeComponent } from '../../componentes/vege/vege.component';
+import { CarneComponent } from '../../componentes/carne/carne.component';
+import { PedidoComponent } from '../../componentes/pedido/pedido.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ItemComponent,
+    AdmComponent,
+    TodoComponent,
+    VegeComponent,
+    CarneComponent,
+    PedidoComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [
+    ItemComponent,
+    AdmComponent,
+    TodoComponent,
+    VegeComponent,
+    CarneComponent,
+    PedidoComponent
   ]
+
 })
 export class MenuModule { }
